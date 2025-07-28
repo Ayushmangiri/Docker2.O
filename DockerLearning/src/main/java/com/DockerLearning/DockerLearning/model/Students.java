5eb17a2650b84b523692fb8b540b6d80c539ff0f
Ayushmangiri <ayushmangiri@gmail.com>
2025-07-28 10:39:59 +0530

    Added Feature

--- DockerLearning/src/main/java/com/DockerLearning/DockerLearning/model/Students.java
+++ DockerLearning/src/main/java/com/DockerLearning/DockerLearning/model/Students.java
@@ -1,14 +1,24 @@
 package com.DockerLearning.DockerLearning.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Students {
-    private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    private String name;
     private int age;
 }
