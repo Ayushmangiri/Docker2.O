c0a4f2a47962b22dbe9054ecdf93103650072897
Ayushmangiri <ayushmangiri@gmail.com>
2025-07-28 09:32:35 +0530

    Added Feature

--- DockerLearning/src/main/java/com/DockerLearning/DockerLearning/controller/StudentController.java
+++ DockerLearning/src/main/java/com/DockerLearning/DockerLearning/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.DockerLearning.DockerLearning.controller;
 
 import com.DockerLearning.DockerLearning.model.Students;
+import com.DockerLearning.DockerLearning.service.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,10 @@ import java.util.List;
 
 @RestController
 public class StudentController {
+
+    @Autowired
+    StudentsService repo;
+
 
     @RequestMapping("/getStudents")
     public List<Students> getStudents() {
--- DockerLearning/src/main/java/com/DockerLearning/DockerLearning/service/StudentsService.java
+++ DockerLearning/src/main/java/com/DockerLearning/DockerLearning/service/StudentsService.java
@@ -1,4 +1,11 @@
 package com.DockerLearning.DockerLearning.service;
 
-public class StudentsService {
+import com.DockerLearning.DockerLearning.model.Students;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+
+public interface StudentsService extends JpaRepository<Students,Integer> {
 }
